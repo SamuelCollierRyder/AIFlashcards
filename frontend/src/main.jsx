f0ee10cb409295f6b126cc569860c982a3c55f59
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-11-09 23:26:18 +0100

    Change component library to daisyUI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,33 +1,10 @@
-import "@cloudscape-design/global-styles/index.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import SignUp from "./SignUp.jsx";
-import LogIn from "./LogIn.jsx";
-import ViewCards from "./ViewCards.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-  {
-    path: "/log-in",
-    element: <LogIn/>,
-  },
-  {
-    path: "/view-cards",
-    element: <ViewCards/>,
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
-);
+)
