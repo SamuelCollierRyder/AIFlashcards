736ad43abda25199a5f5cd7af393da81531ab154
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2024-11-28 18:48:59 +0100

    Fix react warning messages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,37 +10,48 @@ import Index from "./pages/Index.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MathJaxContext } from "better-react-mathjax";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Index />,
+    },
+    {
+      path: "/sign-up",
+      element: <SignUp />,
+    },
+    {
+      path: "/log-in",
+      element: <LogIn />,
+    },
+    {
+      path: "/add-cards",
+      element: <AddCards />,
+    },
+    {
+      path: "/view-cards",
+      element: <ViewCards />,
+    },
+    {
+      path: "/study",
+      element: <Study />,
+    },
+  ],
   {
-    path: "/",
-    element: <Index />,
+    future: {
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+      v7_normalizeFormMethod: true,
+    },
   },
-  {
-    path: "/sign-up",
-    element: <SignUp />,
-  },
-  {
-    path: "/log-in",
-    element: <LogIn />,
-  },
-  {
-    path: "/add-cards",
-    element: <AddCards />,
-  },
-  {
-    path: "/view-cards",
-    element: <ViewCards />,
-  },
-  {
-    path: "/study",
-    element: <Study />,
-  },
-]);
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MathJaxContext>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
     </MathJaxContext>
   </StrictMode>,
 );
--- frontend/src/pages/AddCards.jsx
+++ frontend/src/pages/AddCards.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { MathJax } from "better-react-mathjax";
-import { useNavigate } from "react-router-dom";
 import Layout from "../templates/Layout";
+import { fetchWithAuth } from "../utils/auth";
 
-export default function AddCards({qwe}) {
-  const navigate = useNavigate();
+export default function AddCards() {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault(); // Prevents page reload on form submission
     const event = e.nativeEvent.submitter.id;
     const question = e.target.question.value;
@@ -17,55 +16,67 @@ export default function AddCards({qwe}) {
     if (event === "preview") {
       setQuestion(question);
       setAnswer(answer);
-      document.getElementById("my_modal_1").showModal();
+      document.getElementById("preview_monad").showModal();
     } else if (event === "submit") {
+      console.log(question, answer);
+      await fetchWithAuth(
+        "http://localhost:5000/add-card?question=" +
+          question +
+          "&answer=" +
+          answer,
+      );
+      e.target.question.value = "";
+      e.target.answer.value = "";
     }
   };
 
   return (
-    <Layout authRequired={true} content={
-    <>
-      <dialog id="my_modal_1" className="modal">
-        <div className="modal-box">
-          <strong>Question:</strong>
-          <MathJax>{question}</MathJax>
-          <strong>Answer:</strong>
-          <MathJax>{answer}</MathJax>
-          <div className="modal-action">
-            <form method="dialog">
-              <button className="btn">Close</button>
+    <Layout
+      authRequired={true}
+      content={
+        <>
+          <dialog id="preview_monad" className="modal">
+            <div className="modal-box">
+              <strong>Question:</strong>
+              <MathJax>{question}</MathJax>
+              <strong>Answer:</strong>
+              <MathJax>{answer}</MathJax>
+              <div className="modal-action">
+                <form method="dialog">
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+
+          <div className="sm:w-4/6 lg:w-3/6 mx-auto my-10">
+            <form onSubmit={handleSubmit}>
+              <div className="join-vertical space-y-2">
+                <textarea
+                  className="textarea textarea-bordered w-full resize-none join-item"
+                  placeholder="Question"
+                  spellCheck="true"
+                  id="question"
+                ></textarea>
+                <textarea
+                  className="textarea textarea-bordered w-full resize-none join-item"
+                  placeholder="Answer"
+                  spellCheck="true"
+                  id="answer"
+                ></textarea>
+              </div>
+              <div className="flex justify-end">
+                <button id="preview" className="btn mx-2">
+                  Show preview
+                </button>
+                <button id="submit" type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
             </form>
           </div>
-        </div>
-      </dialog>
-
-      <div className="sm:w-4/6 lg:w-3/6 mx-auto my-10">
-        <form onSubmit={handleSubmit}>
-          <div className="join-vertical space-y-2">
-            <textarea
-              className="textarea textarea-bordered w-full resize-none join-item"
-              placeholder="Question"
-              spellCheck="true"
-              id="question"
-            ></textarea>
-            <textarea
-              className="textarea textarea-bordered w-full resize-none join-item"
-              placeholder="Answer"
-              spellCheck="true"
-              id="answer"
-            ></textarea>
-          </div>
-          <div className="flex justify-end">
-            <button id="preview" className="btn mx-2">
-              Show preview
-            </button>
-            <button id="submit" type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
-    } />
+        </>
+      }
+    />
   );
 }
